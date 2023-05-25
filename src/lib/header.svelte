165aa217a731b2143cfcd8e0cf4798e8de581de8
factoria F5 <tatiyanakedrova94@gmail.com>
2023-05-25 13:35:32 +0200

    Changed the route name (Pages) for (Pagess)

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -4,7 +4,7 @@
 		<li><a href="/">Home</a></li>
 		<li><a href="/prevention">Prevention</a></li>
 		<li><a href="/qurantine">Qurantine</a></li>
-		<li><a href="/pages">Pages</a></li>
+		<li><a href="/pagess">Pages</a></li>
 		<li><a href="/about">About</a></li>
 		<li><a href="/help">Help</a></li>
 		<li id="tracker"><a href="/tracker">Tracker</a></li>
