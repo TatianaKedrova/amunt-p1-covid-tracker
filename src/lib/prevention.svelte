b581f572d4d92f848bc6fbe2e547afe7676eb35b
factoria F5 <tatiyanakedrova94@gmail.com>
2023-06-01 11:30:51 +0200

    Page Prevention. Card as a component. Props inside of card. Icons as a prop as an object

--- src/lib/prevention.svelte
+++ src/lib/prevention.svelte
@@ -1,9 +1,7 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-
-
-
 <script>
+	import PreventionCard from '$lib/prevention_card.svelte';
 </script>
+
 
 <section>
 	<div class="text">
@@ -12,31 +10,26 @@
 	</div>
 
 	<div class="row">
-		<div class="card card1">
-            <div class="icon icon1"><span class="material-symbols-outlined">settings_input_component</span></div>
-			<h2>Cough</h2>
-			<p>
-				Coughing is another key symptom, but its not just any cough, said Shaffner. It should be a
-				dry cought that ypou feel in your chest.
-			</p>
-		</div>
+		<PreventionCard
+			title="cough"
+			description="Coughing is another key symptom, but its not just any cough, said Shaffner. It should be a dry
+		cought that ypou feel in your chest."
+			styleVariant=1
+		/>
 
-		<div class="card card2">
-            <div class="icon icon2"><span class="material-symbols-outlined">deployed_code</span></div>
-			<h2>Difficulty breathing</h2>
-			<p>
-				Shortness of breath can be a third--and very serious--manifistacion of Covid-19, and it can
-				occur on its own, without a cough.
-			</p>
-		</div>
-		<div class="card card3">
-            <div class="icon icon3"><span class="material-symbols-outlined">lock</span></div>
-			<h2>Fever</h2>
-			<p>
-				Fever is a key symptom, experts say. Dont fixate on a number, but know its really not a
-				fever untill your temperature reaches at least 100.
-			</p>
-		</div>
+		<PreventionCard
+			title="Difficulty breathing"
+			description="Shortness of breath can be a third--and very serious--manifistacion of Covid-19, and it can
+			occur on its own, without a cough."
+			styleVariant=2
+		/>
+
+		<PreventionCard
+			title="Fever"
+			description="Fever is a key symptom, experts say. Dont fixate on a number, but know its really not a
+			fever untill your temperature reaches at least 100."
+			styleVariant=3
+		/>
 	</div>
 </section>
 
@@ -67,46 +60,4 @@
 		height: 800px;
 		margin-left: 400px;
 	}
-
-	.card {
-		background-color: #ffffff;
-		display: inline-block;
-		width: 400px;
-		height: 300px;
-		border-radius: 10px;
-
-		box-sizing: border-box;
-		cursor: pointer;
-		margin: 10px 15px;
-		padding: 15px 25px;
-	}
-
-    h2{
-        color: #17165c;
-    }
-
-    p{
-        color: #70709a;
-    }
-
-    .icon{
-        width: 50px;
-        height: 50px;
-        border-radius: 10px;
-        
-    }
-
-    .icon1{
-        background-color: #fa4c9a;
-    }
-
-    .icon2{
-        background-color: #ecbf24;
-    }
-
-    .icon3{
-        background-color: #35bfe7;
-    }
-
-
 </style>
---
+++ src/lib/prevention_card.svelte
@@ -0,0 +1,65 @@
+<script>
+	export let title;
+	export let description;
+	export let styleVariant;
+
+	let iconNames = {
+		1: 'settings_input_component',
+		2: 'deployed_code',
+		3: 'lock'
+	};
+</script>
+
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+/>
+
+<div class="card">
+	<div class={`icon icon${styleVariant}`}>
+		<span class="material-symbols-outlined">{iconNames[styleVariant]}</span>
+	</div>
+	<h2>{title}</h2>
+	<p>{description}</p>
+</div>
+
+<style>
+	.card {
+		background-color: #ffffff;
+		display: inline-block;
+		width: 400px;
+		height: 300px;
+		border-radius: 10px;
+
+		box-sizing: border-box;
+		cursor: pointer;
+		margin: 10px 15px;
+		padding: 15px 25px;
+	}
+
+	h2 {
+		color: #17165c;
+	}
+
+	p {
+		color: #70709a;
+	}
+
+	.icon {
+		width: 50px;
+		height: 50px;
+		border-radius: 10px;
+	}
+
+	.icon1 {
+		background-color: #fa4c9a;
+	}
+
+	.icon2 {
+		background-color: #ecbf24;
+	}
+
+	.icon3 {
+		background-color: #35bfe7;
+	}
+</style>
