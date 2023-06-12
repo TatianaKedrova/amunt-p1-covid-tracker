12a0c023956a282369c3013a48e2184e2432a18e
factoria F5 <tatiyanakedrova94@gmail.com>
2023-06-12 19:20:06 +0200

    Routing

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Homepage from '$lib/homepage.svelte';
+	import Prevention from '$lib/prevention.svelte';
+	import Quarantine from '$lib/quarantine.svelte';
+	import Pages from '$lib/pages.svelte';
+	import About from '$lib/about.svelte';
+	import Help from '$lib/help.svelte';
+</script>
+
+
+<main>
+<Homepage />
+<Prevention />
+<Quarantine />
+<Pages />
+<About />
+<Help />
+</main>
