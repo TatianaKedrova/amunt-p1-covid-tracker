f6a3631da7690162fb3162d0e97c3b5f88c56a44
factoria F5 <tatiyanakedrova94@gmail.com>
2023-06-13 14:28:29 +0200

    Tracker Page - Sidenav Version 1

--- src/routes/tracker/+page.svelte
+++ src/routes/tracker/+page.svelte
@@ -1,8 +1,15 @@
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"/>
+<script>
+	import { base } from '$app/paths';
+</script>
 
-<nav>
+<link
+	rel="stylesheet"
+	href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+/>
+
+<div class="sidenav">
 	<ul>
-		<li><a href="/tracker"><img src="images/covid-defult.svg" alt="Logo" /></a></li>
+		<li><a href="{base}/tracker"><img src="images/covid-defult.svg" alt="Logo" /></a></li>
 		<li><a href="/"><span class="material-symbols-outlined"> pie_chart</span></a></li>
 		<li><a href="/"><span class="material-symbols-outlined">data_table</span></a></li>
 		<li><a href="/"><span class="material-symbols-outlined">dataset</span></a></li>
@@ -13,4 +20,48 @@
 		<li><a href="/"><span class="material-symbols-outlined"> chat_bubble </span></a></li>
 		<li><a href="/"><span class="material-symbols-outlined">group_work</span></a></li>
 	</ul>
-</nav>
+
+
+</div>
+
+<style>
+	.sidenav {
+		width: 6%;
+		height: 100vh;
+		background-color: #f7f8fc;
+
+		position: fixed;
+		border-right: 1px solid #efeff1;
+	}
+
+	img {
+		width: 60px;
+		float: left;
+		padding-bottom: 50px;
+	}
+
+	span {
+		font-size: 40px;
+	}
+
+	.sidenav ul {
+	}
+
+	.sidenav li {
+		list-style: none;
+	}
+
+	.sidenav a {
+		color: #4a65bf;
+		display: block;
+		padding-top: 20px;
+	}
+
+	a:hover {
+		color: hotpink;
+	}
+
+	a:active {
+		color: blue;
+	}
+</style>
